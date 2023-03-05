2fdebf89ef84a10a229dec59be5fb9c17169d8a2
Kodema Vyacheslav <KodemaSlava@yandex.ru>
2023-03-05 20:58:05 +0300

    node js http server

---
+++ njs_server.js
@@ -0,0 +1,13 @@
+const http = require("http");
+const host = 'localhost';
+const port = 80;
+const requestListener = function (req, res) {
+ //   res.setHeader("Content-type", "text/html");
+    res.writeHead(200);
+    res.end("<html><head></head><body> <h1>Java Script Fullstack test server.</h1></body></html>");
+}
+
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+});
